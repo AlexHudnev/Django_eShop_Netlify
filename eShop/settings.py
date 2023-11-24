75d6157207ec7b885ae8c547eee1e52bc27f847b
AlexHudnev <hudneval@mail.ru>
2023-11-24 11:08:05 +0300

    Update settings.py

--- eShop/settings.py
+++ eShop/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$99yzf*g-0db7*m+m%v#x(-@l4+0ch1g5a5vgi7dd=_l#x!ac!'
+ALLOWED_HOSTS = ["*"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
